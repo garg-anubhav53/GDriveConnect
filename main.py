3c07d4f8f15340e7d008e729c8e31e257217e901
Anubhav Garg <garg.anubhav.xyz@gmail.com>
2023-11-17 13:34:34 -0600

    Made some edits to get client working

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from gspread_dataframe import set_with_dataframe
 
-json_path = 'uploadspreadsheet-405418-855ecfddad36.json' # Please set the file for using service account.
+json_path = 'secondkey.json' # Please set the file for using service account.
 
 scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name(json_path, scope)
 client = gspread.authorize(creds)
 
-spreadsheetTitle = 'Test Creation From Python'
+print("Gets to line 11")
+print("This is what is in client: ", client)
+
+spreadsheetTitle = 'Test Creation 2 From Python'
 folderId = '1Ck1IhgOIBsWs05xYGpQs9yYL9oUhkSc2' # Please set the folder ID of the folder in your Google Drive.
 
-workbook = client.create(spreadsheetTitle, folder_id=folderId)
+workbook = client.create(spreadsheetTitle)
+sh = client.open(spreadsheetTitle)
+
+print ("This is what is stored after trying to open a spreadsheet ", sh)
